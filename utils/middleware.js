52fd37046031dc21a7e80c7aace566dd59c15fd2
Assad Isah <nottherealalanturing@gmail.com>
2023-02-13 09:14:37 +0100

    node backbone template

---
+++ utils/middleware.js
@@ -0,0 +1,30 @@
+const logger = "./logger.js";
+
+const requestLogger = (request, response, next) => {
+  logger.info("Method:", request.method);
+  logger.info("path:", request.path);
+  logger.info("body:", request.body);
+  logger.info("- - -:");
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (request, response, next, error) => {
+  logger.error(error.message);
+
+  if (error.name === "CastError")
+    response.status(404).json({ error: "malformatted id" });
+  else if (error.name === "ValidationError")
+    response.status(404).json({ error: error.message });
+
+  next(error);
+};
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+};
