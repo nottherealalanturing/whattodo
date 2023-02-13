2952ba07f0d9da97b2984fb83d1d0b188701eb01
Assad Isah <nottherealalanturing@gmail.com>
2023-02-13 13:40:19 +0100

    add model for task and db

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const database = require("./database");
 const app = express();
+
+database();
+
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
---
+++ utils/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const logger = require("./logger");
+const config = require("./config");
+
+mongoose.set("strictQuery", false);
+logger.info("connecting to database");
+
+module.exports = () => {
+  mongoose
+    .connect(config.MONGODB_URI)
+    .then(() => logger.info("connected to MongoDB"))
+    .catch((error) =>
+      logger.error("error connecting to mongoDB:", error.message)
+    );
+};
